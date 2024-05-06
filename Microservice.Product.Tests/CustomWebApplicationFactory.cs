edeaaef7b96e8cca7763a2747a86e964be257a8c
MauLang18 <maulangbonilla.18@gmail.com>
2024-05-06 09:30:34 -0600

    [ADD] tests

---
+++ Microservice.Product.Tests/CustomWebApplicationFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+
+namespace Microservice.Product.Tests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration(configureBuilder =>
+        {
+            var integrationConfig = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables()
+                .Build();
+
+            configureBuilder.AddConfiguration(integrationConfig);
+        });
+    }
+}
---
+++ Microservice.Product.Tests/Product/Commands/CreateProductCommandTest.cs
@@ -0,0 +1,43 @@
+﻿using MediatR;
+using Microservice.Product.Application.UseCases.Product.Commands.CreateCommand;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microservice.Product.Tests.Product.Commands;
+
+[TestClass]
+public class CreateProductCommandTest
+{
+    private static WebApplicationFactory<Program> _factory = null!;
+    private static IServiceScopeFactory _scopeFactory = null!;
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext _)
+    {
+        _factory = new CustomWebApplicationFactory();
+        _scopeFactory = _factory.Services.GetRequiredService<IServiceScopeFactory>();
+    }
+
+    [TestMethod]
+    public async Task ShouldGetValidationErrors()
+    {
+        using var scope = _scopeFactory.CreateScope();
+        var mediator = scope.ServiceProvider.GetRequiredService<ISender>();
+
+        var command = new CreateProductCommand()
+        {
+            Code = "",
+            Name = "",
+            StockMin = 0,
+            StockMax = 100,
+            UnitSalePrice = 150000,
+            State = 1
+        };
+
+        var expected = false;
+
+        var response = await mediator.Send(command);
+
+        Assert.AreEqual(expected, response.IsSuccess);
+    }
+}
